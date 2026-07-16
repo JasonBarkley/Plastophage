f266c482c7e3b0f2752c37b7ffae1ec44e3e26e3
JasonBarkley <jasonbarkley.writer@gmail.com>
2026-07-16 23:12:16 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'plastophage-v49-debug';
+const CACHE = 'plastophage-v49';
 const ASSETS = ['./','index.html','manifest.webmanifest','icon-192.png','icon-512.png','icon-512-maskable.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))); });
 self.addEventListener('message', e => { if (e.data && e.data.type === 'SKIP_WAITING') self.skipWaiting(); });
